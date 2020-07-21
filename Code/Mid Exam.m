161619217b92b340871f34b6112ccb998f8a0767
abidurcu <49652590+abidurcu@users.noreply.github.com>
2020-07-21 20:58:07 +0600

    Add files via upload

---
+++ Code/Mid Exam.m
@@ -0,0 +1,14 @@
+clc
+clear
+a=input('Enter the value of a=');
+b=input('Enter the value of b=');
+n=input('Enter the value of n=');
+h=(b-a)/n;
+for r=1:1:10
+  x(1)=0;
+  y(1)=x(1)*(2-x(1));
+  x(r+1)=x(r)+h;
+  y(r+1)=x(r+1)*(2-x(r+1));
+end
+I=h/3*((y(1)+y(7))+4*(y(2)+y(4)+y(6))+2*(y(3)+y(5)));
+fprintf('The Integral value is %0.5f',I);
